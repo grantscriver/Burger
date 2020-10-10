135981f5113d61fa37a4b682d099e01107794e8f
Grant Scriver <grantscriver@gmail.com>
2020-10-10 15:53:14 -0500

    Cleaned up and deleted some of the code that I no longer needed

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -42,18 +42,6 @@ $(function() {
       );
     });
   
-    $(".delete-burger").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/burgers/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+    
   });
   
